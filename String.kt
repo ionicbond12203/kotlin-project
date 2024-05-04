aa1ddfcce402432fce5ec9ae80f55d41c92ea95d
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-05-04 18:21:36 +0800

    Create String.kt

---
+++ String.kt
@@ -0,0 +1,17 @@
+fun main () {
+    println("hello world")
+    var userName: String ="Programming in kotlin"
+    userName = "programming in c++"
+   // userName = 5 //causing error
+    println(userName)
+    println("hi,$userName")
+
+    //val - mean cannot change the value after assing it,like const in c++
+    val employeeName: String = "alex"
+    println(employeeName)
+
+    var age : Int = 6
+    println("age: $age")
+
+
+}
