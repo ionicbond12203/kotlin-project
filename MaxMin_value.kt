9237d4224899cab874d825c0325ba269d877a825
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-05-04 18:40:57 +0800

    Update MaxMin_value.kt

--- MaxMin_value.kt
+++ MaxMin_value.kt
@@ -16,6 +16,21 @@ fun main(){
     number = 2147483647 //more than this number causing error
     println(number);
 
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+    println("Byte maximum value is: $myMaxByteValue")   //127
+    println("Byte minimum value is: $myMinByteValue")   //-128
+
+    val myMaxShortValue:Short = Short.MAX_VALUE
+    val myMinShortValue:Short = Short.MIN_VALUE
+    println("Short maximum value is: $myMaxShortValue")
+    println("Short minimum value is: $myMinShortValue")
+
+    val myMaxLongValue:Long =Long.MAX_VALUE
+    val myMinLongValue:Long = Long.MIN_VALUE
+    println("Long maximum value is: $myMaxLongValue")
+    println("Long minimum value is: $myMinLongValue")
+
 
 
 }
