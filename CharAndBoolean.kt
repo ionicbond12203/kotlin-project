832667323b764e3c29c06e9b5b67cb8f7b2c9828
ionic <87571564+ionicbond12203@users.noreply.github.com>
2024-05-05 02:51:57 +0800

    Create CharAndBoolean.kt

---
+++ CharAndBoolean.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>){
+    val myCharValue: Char = 'a'
+    val myBooleanValue:Boolean = true
+    println("myCharValue = $myCharValue")
+    println("myBooleanValue = $myBooleanValue")
+
+}
